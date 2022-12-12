b68a7aa3df97dfce0c24565f19a680612ebc77bf
X-ronin <109928669+X-ronin@users.noreply.github.com>
2022-12-12 17:44:27 -0300

    Update 'setup.sh'.

--- setup.sh
+++ setup.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-### Install progamns ###
-# Zap zsh plugin manager
+### Zap zsh plugin manager ###
 sh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.sh)
 
-### Update ###
-# Slowly neovim plugin manager 
+### Slowly neovim plugin manager ###
 wget -O slowly.zip $(curl -s https://api.github.com/repos/nat-418/slowly.nvim/releases/latest | grep "zipball" | cut -d\" -f4 | grep "https")
 unzip slowly.zip
 cp nat-418-slowly.nvim-d4aaafc/lua/slowly.lua ./nvim/lua/
 rm -rf nat-418-slowly.nvim-d4aaafc/ slowly.zip
 
-# Symlinks
+### Update symlinks ###
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 function update_symlink {
